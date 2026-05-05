01c56fba55eb897507805bb8fdc0ad9c72239b9a
Jin Park <Mids@users.noreply.github.com>
2026-05-05 16:32:08 +0900

    Add PyCharm typing stubs

---
+++ typings/Definitions.pyi
@@ -0,0 +1,15 @@
+from typing import Any
+
+HipName: Any
+LeftHipName: Any
+RightHipName: Any
+LeftShoulderName: Any
+RightShoulderName: Any
+NeckName: Any
+LeftAnkleName: Any
+LeftBallName: Any
+RightAnkleName: Any
+RightBallName: Any
+FULL_BODY_NAMES: Any
+
+def __getattr__(name: str) -> Any: ...
---
+++ typings/ai4animation/__init__.pyi
@@ -0,0 +1,56 @@
+from typing import Any
+
+__version__: str
+
+AI4Animation: Any
+AssetManager: Any
+Entity: Any
+Profiler: Any
+Scene: Any
+Time: Any
+Utility: Any
+
+Actor: Any
+Component: Any
+MeshRenderer: Any
+MotionEditor: Any
+
+ContactModule: Any
+Dataset: Any
+GuidanceModule: Any
+MirrorModule: Any
+Module: Any
+Motion: Any
+MotionModule: Any
+PID: Any
+RootModule: Any
+TimeSeries: Any
+TrackingModule: Any
+
+FABRIK: Any
+
+BVH: Any
+FBX: Any
+GLB: Any
+ModelImporter: Any
+
+Quaternion: Any
+Rotation: Any
+Tensor: Any
+Transform: Any
+Vector3: Any
+
+Plotting: Any
+ReadTensor: Any
+Stats: Any
+Losses: Any
+MultiLayerPerceptron: Any
+
+Camera: Any
+Draw: Any
+GUI: Any
+InputSystem: Any
+LightSystem: Any
+Primitive: Any
+Standalone: Any
+VideoRecorder: Any
